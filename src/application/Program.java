0a40f60fee17b0624dcf28a5289ae635b79d4aa7
Igor Dias <igorverissimo02@hotmail.com>
2024-02-09 20:38:41 -0300

    added error handling

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPossition;
 
@@ -11,16 +13,23 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         Scanner  scanner = new Scanner(System.in);
         while (true) {
-            UI.printBoard(chessMatch.getChessPieces());
-            System.out.println(" ");
-            System.out.print("Source: ");
-            ChessPossition source =  UI.readChessPossition(scanner);
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPossition target = UI.readChessPossition(scanner);
-            UI.clearScreen();
-            chessMatch.performChessMove(source, target);
-
+            try{
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getChessPieces());
+                System.out.println(" ");
+                System.out.print("Source: ");
+                ChessPossition source =  UI.readChessPossition(scanner);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPossition target = UI.readChessPossition(scanner);
+                chessMatch.performChessMove(source, target);
+            }catch (ChessException e){
+                System.out.println(e.getMessage());
+                scanner.nextLine();
+            }catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                scanner.nextLine();
+            }
         }
 
     }
