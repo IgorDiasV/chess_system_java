d1343639673fe34f69d349f9dc49793321ae546d
Igor Dias <igorverissimo02@hotmail.com>
2024-02-17 10:47:21 -0300

    feat: priting captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,25 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import boardgame.BoardException;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPossition;
-
+import chess.ChessPiece;
 public class Program {
 
 	public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
         Scanner  scanner = new Scanner(System.in);
+        List<ChessPiece> captured = new ArrayList<>();  
         while (true) {
             try{
                 UI.clearScreen();
-                UI.printMatch(chessMatch);
+                UI.printMatch(chessMatch, captured);
                 System.out.println(" ");
                 System.out.print("Source: ");
                 ChessPossition source =  UI.readChessPossition(scanner);
@@ -27,7 +30,10 @@ public class Program {
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPossition target = UI.readChessPossition(scanner);
-                chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiecce = chessMatch.performChessMove(source, target);
+                if(capturedPiecce != null){
+                    captured.add(capturedPiecce);
+                }
             }catch (ChessException e){
                 System.out.println(e.getMessage());
                 scanner.nextLine();
